939c711747376befe464c2bad7ce563975e96c84
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2023-12-23 19:01:57 -0700

    assign new user role of customer

--- JattanaNursury/Services/EmailSender.cs
+++ JattanaNursury/Services/EmailSender.cs
@@ -23,34 +23,6 @@ namespace JattanaNursury.Services
 
         public async Task SendEmailAsync(string toEmail, string subject, string email)
         {
-          
-
-            //var gmailClient = new System.Net.Mail.SmtpClient
-            //{
-            //    Host = "smtp.gmail.com",
-            //    Port = 587,
-            //    EnableSsl = true,
-            //    UseDefaultCredentials = false,
-            //    Credentials = new System.Net.NetworkCredential(_username, _password)
-            //};
-
-            //using (var msg = new System.Net.Mail.MailMessage(_fromAddress,toEmail, subject, email))
-            //{
-                
-            //    msg.To.Add(toEmail);
-
-            //    try
-            //    {
-            //        gmailClient.Send(msg);
-           
-            //    }
-            //    catch (Exception)
-            //    {
-            //        // TODO: Handle the exception
-                
-            //    }
-            //}
-
 
             var message = new MimeMessage();
             message.From.Add(new MailboxAddress("Admin", _fromAddress));
