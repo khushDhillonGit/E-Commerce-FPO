76e278e165b4156105c605c77737ad8b0215f77d
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2024-01-06 19:12:38 -0700

    added function to send post request to delete a category

--- JattanaNursury/Controllers/CategoriesController.cs
+++ JattanaNursury/Controllers/CategoriesController.cs
@@ -57,7 +57,7 @@ namespace JattanaNursury.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        //[ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Description")] Category category)
         {
             if (ModelState.IsValid)
@@ -124,42 +124,26 @@ namespace JattanaNursury.Controllers
             }
             return View(category);
         }
-
-        // GET: Categories/Delete/5
-        public async Task<IActionResult> Delete(Guid? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
+        
         // POST: Categories/Delete/5
         [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
+        //[ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
             if (_context.Categories == null)
             {
                 return Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
             }
+
             var category = await _context.Categories.FindAsync(id);
+            if (category == null) { return NotFound(); }
             if (category != null)
             {
                 _context.Categories.Remove(category);
             }
             
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return Ok(new { success= true, url = "/categories/index" });
         }
 
         private bool CategoryExists(Guid id)
---
+++ JattanaNursury/wwwroot/js/categories.js
@@ -0,0 +1,28 @@
+﻿let deletedId = null;
+
+let _postDeleteUrl;
+function initialize(postDeleteUrl)
+{
+    _postDeleteUrl = postDeleteUrl;
+}
+
+function deleteBtnClicked(btn) {
+    const id = $(btn).data('id');
+    deletedId = id;
+    console.log(deletedId);
+}
+
+function deleteItem() {
+    if (deletedId) {
+        $.post(_postDeleteUrl, { id: deletedId }, function (res) {
+            if (res.success) {
+                window.location = res.url;
+            }
+        }).fail(function (res) {
+            alert("Error while deleting, Contact IT");
+        });
+    }
+}
+
+
+
