764dfcf946c0b5e8d194bb376501560c928ea49a
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2024-01-19 21:07:54 -0700

    added category for a business

---
+++ ECommerce/Models/BusinessCategory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Build.Framework;
+
+namespace ECommerce.Models
+{
+    public class BusinessCategory
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
