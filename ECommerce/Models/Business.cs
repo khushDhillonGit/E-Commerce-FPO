1b174d14de4ddb505bd585950ce5e052a95a25e9
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2024-01-19 20:37:03 -0700

    added business model to so user can register their business

---
+++ ECommerce/Models/Business.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerce.Models
+{
+    public class Business
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        public string? Phone { get; set; }
+        public Guid AddressId { get; set; }
+        [ForeignKey(nameof(AddressId))]
+        public Address Address { get; set; }    
+        public Guid OwnerId { get; set; }
+        [ForeignKey(nameof(OwnerId))]
+        public ApplicationUser Owner { get; set; }
+    }
+}
