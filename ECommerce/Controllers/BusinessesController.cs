3f729cbfcbeed06effa55bada1def03343e91954
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2024-01-21 15:36:32 -0700

    added business controller with index method to get all businesses of current user

---
+++ ECommerce/Controllers/BusinessesController.cs
@@ -0,0 +1,40 @@
+﻿using ECommerce.Data;
+using ECommerce.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Controllers
+{
+    [Authorize(Roles = $"{ApplicationRole.BusinessOwner},{ApplicationRole.SuperAdmin}")]
+    public class BusinessesController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public BusinessesController(UserManager<ApplicationUser> userManager, ApplicationDbContext context) : base(userManager,context)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await GetCurrentUserAsync();
+            if (user == null) return Unauthorized();
+            return View(user.Businesses);
+        }
+
+        protected override async Task<ApplicationUser?> GetCurrentUserAsync()
+        {
+            var userName = this.HttpContext?.User?.Identity?.Name;
+            if (userName != null)
+            {
+                return await _context.Users.Include(a => a.Businesses).FirstOrDefaultAsync(a => a.UserName == userName);
+            }
+            return null;
+        }
+
+
+    }
+}
