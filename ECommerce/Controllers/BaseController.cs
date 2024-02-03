91f6a57f22892755c6aae72986de8e671046a49d
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2024-02-02 18:07:00 -0700

    added checks to check the role of current user and ability to store current business Id

--- ECommerce/Controllers/BaseController.cs
+++ ECommerce/Controllers/BaseController.cs
@@ -10,20 +10,53 @@ namespace ECommerce.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ApplicationDbContext _context;
-        public BaseController(UserManager<ApplicationUser> userManager, ApplicationDbContext context) 
+        public BaseController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
         {
             _userManager = userManager;
             _context = context;
         }
 
-        protected virtual async Task<ApplicationUser?> GetCurrentUserAsync() 
+        protected virtual async Task<ApplicationUser?> GetCurrentUserAsync()
         {
             var user = this.HttpContext?.User;
-            if (user != null) 
+            if (user != null)
             {
                 return await _userManager.GetUserAsync(user);
             }
             return null;
+        }
+
+        protected async Task<bool> IsBusinessOwner(ApplicationUser user)
+        {
+            if (user == null || user.Id == Guid.Empty) return false;
+            return await _userManager.IsInRoleAsync(user, ApplicationRole.BusinessOwner);
+        }
+
+        protected async Task<bool> IsCustomer(ApplicationUser user)
+        {
+            if (user == null || user.Id == Guid.Empty) return false;
+            return await _userManager.IsInRoleAsync(user, ApplicationRole.Customer);
+        }
+        protected async Task<bool> IsEmployee(ApplicationUser user)
+        {
+            if (user == null || user.Id == Guid.Empty) return false;
+            return await _userManager.IsInRoleAsync(user, ApplicationRole.Employee);
+        }
+
+        protected Guid? CurrentBusinessId
+        {
+            get
+            {
+                if (HttpContext.Items.TryGetValue(nameof(CurrentBusinessId), out object? val) && val != null)
+                {
+                    return (Guid)val;
+                }
+                return null;
+            }
+            set
+            {
+                HttpContext.Items[nameof(CurrentBusinessId)] = value;
+            }
         }
     }
 }
