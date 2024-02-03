4d521ae28cb18194c95bde3e71b22ed34ab616ee
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2024-02-02 18:07:27 -0700

    routing user when logged in based on their role

--- ECommerce/Controllers/HomeController.cs
+++ ECommerce/Controllers/HomeController.cs
@@ -8,14 +8,31 @@ namespace ECommerce.Controllers
 {
     public class HomeController : BaseController
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
         public HomeController(ILogger<HomeController> logger,UserManager<ApplicationUser> userManager, ApplicationDbContext context) : base(userManager, context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var user = await GetCurrentUserAsync();
+            if (user == null) return NotFound();
+
+            if (await IsBusinessOwner(user)) 
+            {
+                return RedirectToAction("Index","Businesses");
+            }
+
+            if (await IsEmployee(user)) 
+            {
+                var bId = _context.BusinessEmployees.FirstOrDefault(a=>a.Id == user.Id)?.BusinessId;
+                if(bId == null) return NotFound();
+                return RedirectToAction("CurrentBusiness", "Businesses", new { businessId = bId });
+
+            }
             return View();
         }
 
