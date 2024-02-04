91646934d95e201ae40eaff91dc5d6cb2df70df7
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2024-02-04 15:23:09 -0700

    added new constant class to include constant strings

--- ECommerce/Constants.cs
+++ ECommerce/Constants.cs
@@ -3,5 +3,6 @@
     public static class Constants
     {
         public const string CurrentBusinessId  = "CurrentBusinessId"; 
+        public const string CurrentBusinessName  = "CurrentBusinessName"; 
     }
 }
