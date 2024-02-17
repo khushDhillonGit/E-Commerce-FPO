c1d3b669c691a888538217a6a65defe61209d0c8
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2024-02-16 23:38:25 -0700

    added extension to conver product to productVm

---
+++ ECommerce/Extensions/AsProductVM.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce.ViewModels;
+using ECommerce.Models;
+using AutoMapper;
+
+namespace ECommerce.Extensions
+{
+    public static class Extensions 
+    {
+        public static ProductOnSaleViewModel AsProductOnSaleVM(this Product product) 
+        {
+            return new ProductOnSaleViewModel 
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                ImageUrl = product.ImageUrl,
+                CategoryName = product.Category?.Name,
+                BusinessName = product.Category?.Business?.Name
+            };
+        }
+    }
+}
