2c7b17ebdd43e7c7cd972b6e549a7c99ca0747a6
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2024-02-18 23:39:04 -0700

    added price property in product view model

--- ECommerce/ViewModels/ProductOnSaleViewModel.cs
+++ ECommerce/ViewModels/ProductOnSaleViewModel.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }    
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public double Price { get; set; }
         public string? BusinessName { get; set; }
         public string? CategoryName { get; set; }
         public string? ImageUrl { get; set; }
