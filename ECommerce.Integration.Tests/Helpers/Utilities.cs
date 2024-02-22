bb402fc6887d336c4c28c8a74ec7d1f26f86d66f
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2024-02-21 21:56:44 -0700

    added helper class with fake data to seed the sqlite database

---
+++ ECommerce.Integration.Tests/Helpers/Utilities.cs
@@ -0,0 +1,51 @@
+﻿using ECommerce.Data;
+using ECommerce.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace ECommerce.Integration.Tests.Helpers
+{
+    public static class Utilities
+    {
+        public static List<BusinessCategory> GetBusinessCategories()
+        {
+            List<BusinessCategory> categories = new List<BusinessCategory>();
+
+            categories.Add(new BusinessCategory()
+            {
+                Id = Guid.Parse("DFBC6ABC-DFBD-470C-A225-BBB3E051EA6A"),
+                Name = "Retail",
+                Description = "Retail description"
+            });
+
+            categories.Add(new BusinessCategory()
+            {
+                Id = Guid.Parse("3C02BBC1-4ACF-411C-9FCA-B141833E0CE7"),
+                Name = "Retail",
+                Description = "Retail description"
+            });
+
+            return categories;
+        }
+
+        public static Mock<UserManager<ApplicationUser>> MockUserManager()
+        {
+            var userManager = new Mock<UserManager<ApplicationUser>>(
+                new Mock<IUserStore<ApplicationUser>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<IPasswordHasher<ApplicationUser>>().Object,
+                new IUserValidator<ApplicationUser>[0],
+                new IPasswordValidator<ApplicationUser>[0],
+                new Mock<ILookupNormalizer>().Object,
+                new Mock<IdentityErrorDescriber>().Object,
+                new Mock<IServiceProvider>().Object,
+                new Mock<ILogger<UserManager<ApplicationUser>>>().Object);
+
+            return userManager;
+        }
+    }
+}
