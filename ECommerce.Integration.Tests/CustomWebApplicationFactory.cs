43c1bf158ded50c326c9f00ca8714f33e902660f
khushwindersingh1947 <khushwindersinghdhillon79@gmail.com>
2024-02-21 21:56:21 -0700

    custom web application factory to get into pipline and inject sqlite to perform integration testing

---
+++ ECommerce.Integration.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,55 @@
+﻿
+using ECommerce.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Integration.Tests
+{
+    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services => 
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+                var dbDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbConnection));
+                if (dbDescriptor != null)
+                {
+                    services.Remove(dbDescriptor);
+                }
+
+                services.AddSingleton<DbConnection>(container =>
+                {
+                    var connection = new SqliteConnection("DataSource=:memory:");
+                    connection.Open();
+
+                    return connection;
+                });
+
+
+
+                services.AddDbContext<ApplicationDbContext>((ct, options) => { 
+                    var cn = ct.GetRequiredService<DbConnection>();
+                    options.UseSqlite(cn);
+                });
+
+                using var scope = services.BuildServiceProvider().CreateScope();
+                var db = scope.ServiceProvider.GetService<ApplicationDbContext>() ?? throw new Exception();
+          
+            });
+        }
+    }
+}
